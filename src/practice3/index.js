75e019dee7531ca36284773454695ae8721b3444
“pyqpyqpyq” <alwayswannaflyx@gmail.com>
2020-11-11 16:11:56 +0800

    [pyq] implement the tasks

--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,1 +1,13 @@
-export const getPoetry
+export const getPoetry = () => {
+  return fetch("https://v1.jinrishici.com/all.json")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      const { origin, author, content } = data;
+      return [origin, author, content];
+    });
+};
